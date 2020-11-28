bbda0434e1ceca8826f4e1dd62bb5dceed396264
Aarchu-B <archanabaral2000@gmail.com>
2020-11-28 12:20:22 +0545

    create Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import React from "react"
-
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 function App() {
   return (
-   <div>Hello World</div>
-    
+    <Router>
+    <div className="grid-container">
+    <header className="row">
+     <div >
+         <a className="brand" href="/">Clothing Line</a>
+     </div>
+     <div>
+         <a href="/cart">Cart</a>
+         <a href="/signin">Sign In</a>
+     </div>
+    </header>
+    <main>
+      <Route  path="/" component={HomeScreen} exact></Route>
+      <Route  path="/product/:id" component={ProductScreen}></Route>
+      
+    </main>
+    <footer className="row center">
+       All right reserved
+    </footer>
+</div>
+</Router>
   );
 }
 
 export default App;
+
