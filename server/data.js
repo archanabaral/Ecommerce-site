92092bd287af8eab46a1325ec7619ab6653cf3e1
Aarchu-B <archanabaral2000@gmail.com>
2020-11-28 11:20:00 +0545

    create mockData file

---
+++ server/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products:[
+        {
+        
+        name:'Formal pant and top',
+        category:'formals',
+        image:'/images/p-1.jpg',
+        price:120,
+        countInStock:10,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:"high quality product"
+        
+        },
+        {
+        
+        name:'Formal pant and top',
+        category:'formals',
+        image:'/images/p-1.jpg',
+        price:120,
+        countInStock:2,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:"high quality product"
+        },
+        {
+        
+        name:'Formal pant and top',
+        category:'formals',
+        image:'/images/p-1.jpg',
+        price:120,
+        countInStock:10,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:"high quality product"
+        },
+        {
+        
+        name:'Formal pant and top',
+        category:'formals',
+        image:'/images/p-1.jpg',
+        price:120,
+        countInStock:10,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:"high quality product"
+        }, 
+        {
+        
+        name:'Formal pant and top',
+        category:'formals',
+        image:'/images/p-1.jpg',
+        price:120,
+        countInStock:5,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:"high quality product"
+        },
+        {
+    
+        name:'Formal pant and top',
+        category:'formals',
+        image:'/images/p-1.jpg',
+        price:120,
+        countInStock:0,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10,
+        description:"high quality product"
+        }
+
+    ]
+}
+export default data
