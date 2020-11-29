6311fd150e4c0e9fadef13e1ba2bd2146042d310
Aarchu-B <archanabaral2000@gmail.com>
2020-11-29 10:55:38 +0545

    set initialState to cart

--- client/src/store.js
+++ client/src/store.js
@@ -5,7 +5,11 @@ import {
     productListReducer,productDetailsReducer
   } from './reducers/productReducers';
 
-  const initialState = {};
+  const initialState = {
+    cart:{
+      cartItems:localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[]
+    }
+  };
 
   const reducer = combineReducers({
     productList: productListReducer,
