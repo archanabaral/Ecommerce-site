1f8a89d608728022d38e26c52ab23dc2c2f839d7
Aarchu-B <archanabaral2000@gmail.com>
2020-11-29 19:32:22 +0545

    add link to signin

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,39 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 import CartScreen from "./screens/CartScreen"
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
+import SignninScreen from "./screens/SignninScreen"
 function App() {
 
   const cart= useSelector(state => state.cart)
   const {cartItems} = cart
   return (
     <Router>
-    <div className="grid-container">
-    <header className="row">
-     <div >
-         <Link className="brand" to="/">Clothing Line</Link>
-     </div>
-     <div>
-         <Link to ="/cart">Cart
-         {cartItems.length > 0 &&(
-           <span className="badge">{cartItems.length}</span>
-         )}
-         </Link>
-         <Link to="/signin">Sign In</Link>
-     </div>
-    </header>
-    <main>
-      <Route  path="/" component={HomeScreen} exact></Route>
-      <Route  path="/product/:id" component={ProductScreen}></Route>
-      <Route path="/cart/:id?" component={CartScreen}></Route>
-    </main>
-    <footer className="row center">
-       All right reserved
-    </footer>
-</div>
-</Router>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              Clothing Line
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
+          </div>
+        </header>
+        <main>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/signin" component={SignninScreen}></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </Router>
   );
 }
 
