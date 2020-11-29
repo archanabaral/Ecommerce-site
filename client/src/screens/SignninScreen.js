36de0836652f93dbd1a9f75cca134d392e20d99c
Aarchu-B <archanabaral2000@gmail.com>
2020-11-29 21:28:08 +0545

    create SigninScreen

---
+++ client/src/screens/SignninScreen.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signin } from "../actions/userActions";
+import MessageBox from "../components/MessageBox";
+
+export default function SignninScreen(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const redirect = props.location.search
+    ? props.location.search.split("=")[1]
+    : "/";
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo, error } = userSignin;
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(signin(email, password));
+  };
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    }
+  }, [userInfo, redirect, props.history]);
+
+  return (
+    <div>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Sign In</h1>
+        </div>
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
+        <div>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Sign In
+          </button>
+        </div>
+        <div>
+          <label />
+          <div>
+            New Customer? <Link to="/register">Create your account</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
