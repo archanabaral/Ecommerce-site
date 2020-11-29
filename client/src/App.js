25335be9058e26e39d8a922caf1aa8a7e66fd79c
Aarchu-B <archanabaral2000@gmail.com>
2020-11-29 11:07:32 +0545

    show number of item in cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { useSelector } from "react-redux"
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 import CartScreen from "./screens/CartScreen"
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
 function App() {
+
+  const cart= useSelector(state => state.cart)
+  const {cartItems} = cart
   return (
     <Router>
     <div className="grid-container">
@@ -12,7 +16,11 @@ function App() {
          <Link className="brand" to="/">Clothing Line</Link>
      </div>
      <div>
-         <Link to ="/cart">Cart</Link>
+         <Link to ="/cart">Cart
+         {cartItems.length > 0 &&(
+           <span className="badge">{cartItems.length}</span>
+         )}
+         </Link>
          <Link to="/signin">Sign In</Link>
      </div>
     </header>
