96ce5213868504a6399ae086ec8fbb93e8c5bcad
Aarchu-B <archanabaral2000@gmail.com>
2020-11-29 10:39:02 +0545

    add Link

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 import CartScreen from "./screens/CartScreen"
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
@@ -9,11 +9,11 @@ function App() {
     <div className="grid-container">
     <header className="row">
      <div >
-         <a className="brand" href="/">Clothing Line</a>
+         <Link className="brand" to="/">Clothing Line</Link>
      </div>
      <div>
-         <a href="/cart">Cart</a>
-         <a href="/signin">Sign In</a>
+         <Link to ="/cart">Cart</Link>
+         <Link to="/signin">Sign In</Link>
      </div>
     </header>
     <main>
