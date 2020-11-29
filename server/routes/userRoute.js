15cc628bd0e03e4373081a71a8076abd519d82e6
Aarchu-B <archanabaral2000@gmail.com>
2020-11-29 14:50:08 +0545

    create route to seed and get data

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,22 @@
-import express from "express"
-import data from "../data.js"
-import User from "../models/userModel.js"
+import express from "express";
+import  expressAsyncHandler  from "express-async-handler";
+import data from "../data.js";
+import User from "../models/userModel.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.get('/seed', async(req,res) => {
-    const seedUser= await User.insertMany(data.users)
-    res.send(seedUser)
-})
-export default userRouter
+userRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
+    const seedUser = await User.insertMany(data.users);
+    res.send(seedUser);
+  })
+);
+userRouter.get(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+  })
+);
+export default userRouter;
