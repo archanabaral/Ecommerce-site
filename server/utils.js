4e8d9f06e806fa059911a17f3c9a3f6d962ef16b
Aarchu-B <archanabaral2000@gmail.com>
2020-12-01 14:51:44 +0545

    verify token

--- server/utils.js
+++ server/utils.js
@@ -1,4 +1,4 @@
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 export const generateToken = (user) => {
   //sign method helps to generate token first parameter is object we are gona use to generate token second parameter is jsonwebtoken secret it is like a key to encrypt data and generate a token
   return jwt.sign(
@@ -11,7 +11,26 @@ export const generateToken = (user) => {
     // eslint-disable-next-line no-undef
     process.env.JWT_SECRET,
     {
-      expiresIn: "10m",
+      expiresIn: "30d",
     }
   );
+};
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); //Bearer xxxxxxxxx
+    //decrypt token
+
+    // eslint-disable-next-line no-undef
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(400).send({ msg: "Invalid Token" });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ msg: "No Token" });
+  }
 };
