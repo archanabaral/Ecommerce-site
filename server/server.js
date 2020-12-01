ff0540cc028550d3a01be372ef5c8f73d78268f0
Aarchu-B <archanabaral2000@gmail.com>
2020-12-01 14:52:14 +0545

    create orders route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRouter from "./routes/productRoute.js";
 import userRouter from "./routes/userRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 //load env variables
 dotenv.config({ path: "./config/config.env" });
@@ -13,11 +14,12 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true }))
+app.use(express.urlencoded({ extended: true }));
 
 //route
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
